f64f75b7562c89064811b00178ad72d9767ca226
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-09-21 20:39:03 -0600

    commit tree cf23e9dd057c7466abe5d5e70b807e01ad4fff45

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
   # GET /plants
   def index
     plants = Plant.all
@@ -8,7 +10,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plants
     render json: plant
   end
 
@@ -18,9 +20,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = find_plants
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DESTROY /plants/:id
+  def destroy
+    plant = find_plants
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def find_plants
+    Plant.find_by(id: params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_not_found
+    render json: { error: "plant not found" }, status: :not_found
   end
 end
